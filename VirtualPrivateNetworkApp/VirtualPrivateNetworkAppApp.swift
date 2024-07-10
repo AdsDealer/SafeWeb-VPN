d49fae4649ce936a5947d0751240edbf93ebfc60
dieofcode <fr0stsone878@gmail.com>
2024-07-10 19:26:34 +0500

    Initial Commit

---
+++ VirtualPrivateNetworkApp/VirtualPrivateNetworkAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  VirtualPrivateNetworkAppApp.swift
+//  VirtualPrivateNetworkApp
+//
+//  Created by Иван Свирский on 10.07.24.
+//
+
+import SwiftUI
+
+@main
+struct VirtualPrivateNetworkAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
